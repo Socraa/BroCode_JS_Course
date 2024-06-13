df96c3fdfc7462697aac91d8c910cf31b08c3e58
Jai Dyelle Dela Cruz <Jaidyelle12@gmail.com>
2024-06-13 20:28:26 +0800

    Dates

---
+++ date-Objects/script.js
@@ -0,0 +1,7 @@
+// Date arguments(Year, Month, Day, Hour, Minute, Second, Ms)
+let date = new Date();
+
+console.log(date.toDateString()); // Outputs date in readable format
+console.log(date.toTimeString()); // Outputs time in readable format
+console.log(date.toISOString());  // Outputs date in ISO 8601 format
+
