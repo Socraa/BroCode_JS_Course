0c13db0a41daddd3cbe799b7bf1d4fbdc1a95af7
Jai Dyelle Dela Cruz <Jaidyelle12@gmail.com>
2024-06-14 22:36:49 +0800

    fucking stopwatch

--- Digital-Clock/script.js
+++ Digital-Clock/script.js
@@ -3,7 +3,7 @@ function updateClock(){
     const now = new Date();
     let hours = now.getHours().toString().padStart(2,0);
     let mid = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12 || 12;
+    hours = hours % 12 || 12; // it is like saying if hours's modulus is 0 then return 12 instead of modulus
     hours = hours.toString().padStart(2,0);
     const minutes = now.getMinutes().toString().padStart(2,0);
     const seconds = now.getSeconds().toString().padStart(2,0);
